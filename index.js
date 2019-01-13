87fdc604781e75e8e8cddaa84aa343628d8878ab
Charles Duarte <charlesduarte019@gmail.com>
2019-01-13 11:19:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var port = 3000;
 app.get('/', function(req, res){
     res.send('Fala galerinha!');
 });
+
 app.listen(port, function(){
-    console.log('Rodando na porta '+port)
+    console.log('Rodando na porta %s', port)
 });
